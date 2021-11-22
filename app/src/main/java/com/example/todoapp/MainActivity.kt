23263ac42e9e2124e71e12a6cda9506e28ed02b7
Edyta Szarowska <edyta.szarowska@interia.pl>
2021-11-22 22:02:22 +0100

    init project and add >Toolbar

---
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.todoapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.Toolbar
+import android.view.Menu
+
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+}
